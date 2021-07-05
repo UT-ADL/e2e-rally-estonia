9aec8049726c42f982db2d22cf47cd9815f22e99
Romet Aidla <rometaidla@gmail.com>
2021-07-05 10:38:41 +0300

    Notebook for creating (autonomous) driving video added.

--- data_extract/image_extractor.py
+++ data_extract/image_extractor.py
@@ -22,18 +22,19 @@ class NvidiaDriveImporter:
         # front 120'
         self.front_wide_camera_topic = "/interfacea/link2/image/compressed"
         # front 60'
-        self.front_narrow_camera_topic = "/interfacea/link3/image/compressed"
+        #self.front_narrow_camera_topic = "/interfacea/link3/image/compressed"
         # steering topic
         self.steer_topic = '/pacmod/parsed_tx/steer_rpt'
+        # autonomy
+        self.autonomy_topic = '/pacmod/as_tx/enabled'
 
-        self.camera_topics = [self.left_camera_topic, self.right_camera_topic, self.front_wide_camera_topic, self.front_narrow_camera_topic]
-        self.topics = self.camera_topics + [self.steer_topic]
+        self.camera_topics = [self.left_camera_topic, self.right_camera_topic, self.front_wide_camera_topic]
+        self.topics = self.camera_topics + [self.steer_topic, self.autonomy_topic]
 
         self.topic_to_camera_name_map = {
             self.left_camera_topic: "left",
             self.right_camera_topic: "right",
-            self.front_wide_camera_topic: "front_wide",
-            self.front_narrow_camera_topic: "front_narrow"
+            self.front_wide_camera_topic: "front_wide"
         }
 
     def import_bags(self):
@@ -46,8 +47,8 @@ class NvidiaDriveImporter:
             self.left_camera_topic: 0,
             self.right_camera_topic: 0,
             self.front_wide_camera_topic: 0,
-            self.front_narrow_camera_topic: 0,
-            self.steer_topic: 0
+            self.steer_topic: 0,
+            self.autonomy_topic: 0
         }
 
         bag = rosbag.Bag(bag_file, "r")
@@ -67,31 +68,43 @@ class NvidiaDriveImporter:
         steering_dict = defaultdict(list)
         camera_dict = defaultdict(list)
 
+        autonomous = False
+        autonomy_changed = False
+
         progress = tqdm(total=bag.get_message_count(self.camera_topics))
         for topic, msg, ts in bag.read_messages(topics=self.topics):
+
             stats[topic] += 1
 
-            msg_timestamp = msg.header.stamp.to_nsec()
-            if topic == self.steer_topic:
-                steering_dict["timestamp"].append(msg_timestamp)
-                steering_dict["steering_angle"].append(msg.manual_input)
-            elif topic in self.camera_topics:
-                camera_name = self.topic_to_camera_name_map[topic]
-                output_folder = root_folder / camera_name
-                camera_dict["timestamp"].append(msg_timestamp)
-                camera_dict["camera"].append(camera_name)
-                image_name = f"{msg_timestamp}.jpg"
-                camera_dict["filename"].append(str(Path(output_folder.stem) / image_name))
-                cv_img = bridge.compressed_imgmsg_to_cv2(msg)
-                cv2.imwrite(str(output_folder / image_name), cv_img)
-                progress.update(1)
+            if topic == self.autonomy_topic:
+                autonomy_changed = autonomous != msg.data
+                autonomous = msg.data
+                if autonomy_changed:
+                    print("Autonomy changed to ", autonomous)
+            else:
+                msg_timestamp = msg.header.stamp.to_nsec()
+
+                if topic == self.steer_topic:
+                    steering_dict["timestamp"].append(msg_timestamp)
+                    steering_dict["steering_angle"].append(msg.manual_input)
+
+                elif topic in self.camera_topics:
+                    camera_name = self.topic_to_camera_name_map[topic]
+                    output_folder = root_folder / camera_name
+                    camera_dict["timestamp"].append(msg_timestamp)
+                    camera_dict["autonomous"].append(autonomous)
+                    camera_dict["camera"].append(camera_name)
+                    image_name = f"{msg_timestamp}.jpg"
+                    camera_dict["filename"].append(str(Path(output_folder.stem) / image_name))
+                    cv_img = bridge.compressed_imgmsg_to_cv2(msg)
+                    cv2.imwrite(str(output_folder / image_name), cv_img)
+                    progress.update(1)
 
         bag.close()
 
-        camera_df = pd.DataFrame(data=camera_dict, columns=["timestamp", "camera", "filename"])
+        camera_df = pd.DataFrame(data=camera_dict, columns=["timestamp", "camera", "filename", "autonomous"])
         self.create_timestamp_index(camera_df)
 
-        front_narrow_camera_df = self.create_camera_df(camera_df, "front_narrow")
         front_wide_camera_df = self.create_camera_df(camera_df, "front_wide")
         left_camera_df = self.create_camera_df(camera_df, "left")
         right_camera_df = self.create_camera_df(camera_df, "right")
@@ -101,10 +114,11 @@ class NvidiaDriveImporter:
 
         merged = functools.reduce(lambda left, right:
                                   pd.merge(left, right, how='outer', left_index=True, right_index=True),
-                                  [front_narrow_camera_df, front_wide_camera_df, left_camera_df, right_camera_df,
-                                   steering_df])
+                                  [front_wide_camera_df, left_camera_df, right_camera_df, steering_df])
         merged.interpolate(method='time', inplace=True)
-        filtered_df = merged.loc[front_narrow_camera_df.index]
+
+        print(merged)
+        filtered_df = merged.loc[front_wide_camera_df.index]
         filtered_df.to_csv(root_folder / "frames.csv", header=True)
 
     def create_timestamp_index(self, df):
--- dataloading/nvidia.py
+++ dataloading/nvidia.py
@@ -104,11 +104,26 @@ class NvidiaDataset(ImageDataset):
         data = defaultdict(list)
 
         for dataset_path in dataset_paths:
-            camera_images, steering_angles = self.read_dataset(dataset_path, camera)
+            camera_images, steering_angles, autonomous = self.read_dataset(dataset_path, camera)
             data["images"].extend(camera_images)
             data["steering_angle"].extend(steering_angles + steering_correction)
+            data["autonomous"].extend(autonomous)
 
         super().__init__(data["images"], data["steering_angle"], transform)
+        self.autonomous = data["autonomous"]
+
+    def __getitem__(self, idx):
+        image = torchvision.io.read_image(self.image_paths[idx])
+        data = {
+            'image': image,
+            'steering_angle': np.array(self.steering_angles[idx]),
+            'autonomous': np.array(self.autonomous[idx])
+        }
+
+        if self.transform:
+            data = self.transform(data)
+
+        return data
 
     def read_dataset(self, dataset_path, camera):
         frames_df = pd.read_csv(dataset_path / "frames.csv")
@@ -117,8 +132,9 @@ class NvidiaDataset(ImageDataset):
         frames_df = frames_df[frames_df['right_filename'].notna()]
         frames_df = frames_df[frames_df['front_wide_filename'].notna()]
         steering_angles = frames_df["steering_angle"].to_numpy()
+        autonomous = frames_df["autonomous"].to_numpy()
 
         camera_images = frames_df[f"{camera}_filename"].to_numpy()
         camera_images = [str(dataset_path / image_path) for image_path in camera_images]
 
-        return camera_images, steering_angles
+        return camera_images, steering_angles, autonomous
