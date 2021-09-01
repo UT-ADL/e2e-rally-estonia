658638db9e2245a06901b5712d600e575266dcb1
Romet Aidla <romet.aidla@gmail.com>
2021-09-01 15:47:57 +0300

    Added vehicle speed and turn signal extractions.

--- data_extract/image_extractor.py
+++ data_extract/image_extractor.py
@@ -23,13 +23,13 @@ class NvidiaDriveImporter:
         self.front_wide_camera_topic = "/interfacea/link2/image/compressed"
         # front 60'
         #self.front_narrow_camera_topic = "/interfacea/link3/image/compressed"
-        # steering topic
         self.steer_topic = '/pacmod/parsed_tx/steer_rpt'
-        # autonomy
+        self.speed_topic = '/pacmod/parsed_tx/vehicle_speed_rpt'
+        self.turn_topic = '/pacmod/parsed_tx/turn_rpt'
         self.autonomy_topic = '/pacmod/as_tx/enabled'
 
         self.camera_topics = [self.left_camera_topic, self.right_camera_topic, self.front_wide_camera_topic]
-        self.topics = self.camera_topics + [self.steer_topic, self.autonomy_topic]
+        self.topics = self.camera_topics + [self.steer_topic, self.speed_topic, self.turn_topic, self.autonomy_topic]
 
         self.topic_to_camera_name_map = {
             self.left_camera_topic: "left",
@@ -48,6 +48,8 @@ class NvidiaDriveImporter:
             self.right_camera_topic: 0,
             self.front_wide_camera_topic: 0,
             self.steer_topic: 0,
+            self.speed_topic: 0,
+            self.turn_topic: 0,
             self.autonomy_topic: 0
         }
 
@@ -66,6 +68,8 @@ class NvidiaDriveImporter:
             camera_folder.mkdir()
 
         steering_dict = defaultdict(list)
+        speed_dict = defaultdict(list)
+        turn_dict = defaultdict(list)
         camera_dict = defaultdict(list)
 
         autonomous = False
@@ -87,6 +91,14 @@ class NvidiaDriveImporter:
                 if topic == self.steer_topic:
                     steering_dict["timestamp"].append(msg_timestamp)
                     steering_dict["steering_angle"].append(msg.manual_input)
+
+                elif topic == self.speed_topic:
+                    speed_dict["timestamp"].append(msg_timestamp)
+                    speed_dict["vehicle_speed"].append(msg.vehicle_speed)
+
+                elif topic == self.turn_topic:
+                    turn_dict["timestamp"].append(msg_timestamp)
+                    turn_dict["turn_signal"].append(msg)
 
                 elif topic in self.camera_topics:
                     camera_name = self.topic_to_camera_name_map[topic]
@@ -112,9 +124,15 @@ class NvidiaDriveImporter:
         steering_df = pd.DataFrame(data=steering_dict, columns=["timestamp", "steering_angle"])
         self.create_timestamp_index(steering_df)
 
+        speed_df = pd.DataFrame(data=speed_dict, columns=["timestamp", "vehicle_speed"])
+        self.create_timestamp_index(speed_df)
+
+        turn_df = pd.DataFrame(data=turn_dict, columns=["timestamp", "turn_signal"])
+        self.create_timestamp_index(turn_df)
+
         merged = functools.reduce(lambda left, right:
                                   pd.merge(left, right, how='outer', left_index=True, right_index=True),
-                                  [front_wide_camera_df, left_camera_df, right_camera_df, steering_df])
+                                  [front_wide_camera_df, left_camera_df, right_camera_df, turn_df, speed_df, steering_df])
         merged.interpolate(method='time', inplace=True)
 
         print(merged)
