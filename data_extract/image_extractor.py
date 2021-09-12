daa3cf175f1b23b88053a67f480977cc111843f7
Romet Aidla <romet.aidla@gmail.com>
2021-09-13 00:39:36 +0300

    Turn signal extraction from bag files added.

--- data_extract/image_extractor.py
+++ data_extract/image_extractor.py
@@ -17,25 +17,32 @@ class NvidiaDriveImporter:
         self.extract_dir = extract_dir
 
         # left 120'
+        self.left_camera_topic_old = "/interfacea/link0/image_raw/compressed"
         self.left_camera_topic = "/interfacea/link0/image/compressed"
         # right 120'
+        self.right_camera_topic_old = "/interfacea/link1/image_raw/compressed"
         self.right_camera_topic = "/interfacea/link1/image/compressed"
         # front 120'
+        self.front_wide_camera_topic_old = "/interfacea/link2/image_raw/compressed"
         self.front_wide_camera_topic = "/interfacea/link2/image/compressed"
         # front 60'
         #self.front_narrow_camera_topic = "/interfacea/link3/image/compressed"
         self.steer_topic = '/pacmod/parsed_tx/steer_rpt'
         self.speed_topic = '/pacmod/parsed_tx/vehicle_speed_rpt'
-        #self.turn_topic = '/pacmod/parsed_tx/turn_rpt'
+        self.turn_topic = '/pacmod/parsed_tx/turn_rpt'
         self.autonomy_topic = '/pacmod/as_tx/enabled'
 
-        self.camera_topics = [self.left_camera_topic, self.right_camera_topic, self.front_wide_camera_topic]
-        #self.topics = self.camera_topics + [self.steer_topic, self.speed_topic, self.turn_topic, self.autonomy_topic]
-        self.topics = self.camera_topics + [self.steer_topic, self.speed_topic, self.autonomy_topic]
+        self.camera_topics = [self.left_camera_topic_old, self.left_camera_topic,
+                              self.right_camera_topic_old, self.right_camera_topic,
+                              self.front_wide_camera_topic_old, self.front_wide_camera_topic]
+        self.topics = self.camera_topics + [self.steer_topic, self.speed_topic, self.turn_topic, self.autonomy_topic]
 
         self.topic_to_camera_name_map = {
+            self.left_camera_topic_old: "left",
             self.left_camera_topic: "left",
+            self.right_camera_topic_old: "right",
             self.right_camera_topic: "right",
+            self.front_wide_camera_topic_old: "front_wide",
             self.front_wide_camera_topic: "front_wide"
         }
 
@@ -46,12 +53,15 @@ class NvidiaDriveImporter:
 
     def import_bag(self, bag_file):
         stats = {
+            self.left_camera_topic_old: 0,
             self.left_camera_topic: 0,
+            self.right_camera_topic_old: 0,
             self.right_camera_topic: 0,
+            self.front_wide_camera_topic_old: 0,
             self.front_wide_camera_topic: 0,
             self.steer_topic: 0,
             self.speed_topic: 0,
-            #self.turn_topic: 0,
+            self.turn_topic: 0,
             self.autonomy_topic: 0
         }
 
@@ -70,11 +80,11 @@ class NvidiaDriveImporter:
         for camera_topic in self.camera_topics:
             camera_name = self.topic_to_camera_name_map[camera_topic]
             camera_folder = root_folder / camera_name
-            camera_folder.mkdir()
+            camera_folder.mkdir(exist_ok=True)
 
         steering_dict = defaultdict(list)
         speed_dict = defaultdict(list)
-        #turn_dict = defaultdict(list)
+        turn_dict = defaultdict(list)
         camera_dict = defaultdict(list)
 
         autonomous = False
@@ -96,22 +106,20 @@ class NvidiaDriveImporter:
                 if topic == self.steer_topic:
                     steering_dict["timestamp"].append(msg_timestamp)
                     steering_dict["steering_angle"].append(msg.manual_input)
+                    steering_dict["autonomous"].append(autonomous)
 
                 elif topic == self.speed_topic:
                     speed_dict["timestamp"].append(msg_timestamp)
                     speed_dict["vehicle_speed"].append(msg.vehicle_speed)
 
-                # elif topic == self.turn_topic:
-                #     turn_dict["timestamp"].append(msg_timestamp)
-                #     #print("-----------------")
-                #     #print("turn_topic: ", msg.enabled)
-                #     turn_dict["turn_signal"].append(msg.enabled)
+                elif topic == self.turn_topic:
+                    turn_dict["timestamp"].append(msg_timestamp)
+                    turn_dict["turn_signal"].append(int(msg.output))
 
                 elif topic in self.camera_topics:
                     camera_name = self.topic_to_camera_name_map[topic]
                     output_folder = root_folder / camera_name
                     camera_dict["timestamp"].append(msg_timestamp)
-                    camera_dict["autonomous"].append(autonomous)
                     camera_dict["camera"].append(camera_name)
                     image_name = f"{msg_timestamp}.jpg"
                     camera_dict["filename"].append(str(Path(output_folder.stem) / image_name))
@@ -121,29 +129,27 @@ class NvidiaDriveImporter:
 
         bag.close()
 
-        camera_df = pd.DataFrame(data=camera_dict, columns=["timestamp", "camera", "filename", "autonomous"])
+        camera_df = pd.DataFrame(data=camera_dict, columns=["timestamp", "camera", "filename"])
         self.create_timestamp_index(camera_df)
 
         front_wide_camera_df = self.create_camera_df(camera_df, "front_wide")
         left_camera_df = self.create_camera_df(camera_df, "left")
         right_camera_df = self.create_camera_df(camera_df, "right")
 
-        steering_df = pd.DataFrame(data=steering_dict, columns=["timestamp", "steering_angle"])
+        steering_df = pd.DataFrame(data=steering_dict, columns=["timestamp", "steering_angle", "autonomous"])
         self.create_timestamp_index(steering_df)
 
         speed_df = pd.DataFrame(data=speed_dict, columns=["timestamp", "vehicle_speed"])
         self.create_timestamp_index(speed_df)
 
-        # turn_df = pd.DataFrame(data=turn_dict, columns=["timestamp", "turn_signal"])
-        # self.create_timestamp_index(turn_df)
+        turn_df = pd.DataFrame(data=turn_dict, columns=["timestamp", "turn_signal"])
+        self.create_timestamp_index(turn_df)
 
         merged = functools.reduce(lambda left, right:
                                   pd.merge(left, right, how='outer', left_index=True, right_index=True),
-                                  #[front_wide_camera_df, left_camera_df, right_camera_df, turn_df, speed_df, steering_df])
-                                  [front_wide_camera_df, left_camera_df, right_camera_df, speed_df, steering_df])
+                                  [front_wide_camera_df, left_camera_df, right_camera_df, turn_df, speed_df, steering_df])
         merged.interpolate(method='time', inplace=True)
 
-        print(merged)
         filtered_df = merged.loc[front_wide_camera_df.index]
         filtered_df.to_csv(root_folder / "frames.csv", header=True)
 
