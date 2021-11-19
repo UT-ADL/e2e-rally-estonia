630e0cfe847d91d155e6dfd8e5199f03ab8fc357
Romet Aidla <romet.aidla@gmail.com>
2021-11-19 11:14:59 +0200

    Updated data loaders with new bags.

--- dataloading/nvidia.py
+++ dataloading/nvidia.py
@@ -79,7 +79,7 @@ class NvidiaDataset(Dataset):
     N_WAYPOINTS = 5
     CAP_WAYPOINTS = 10
 
-    def __init__(self, dataset_paths, transform=None, camera="front_wide", name="Nvidia dataset"):
+    def __init__(self, dataset_paths, transform=None, camera="front_wide", name="Nvidia dataset", filter_turns=False):
         self.name = name
         self.dataset_paths = dataset_paths
         self.transform = transform
@@ -87,6 +87,10 @@ class NvidiaDataset(Dataset):
 
         datasets = [self.read_dataset(dataset_path, camera) for dataset_path in dataset_paths]
         self.frames = pd.concat(datasets)
+
+        if filter_turns:
+            print("Filtering turns with blinker signal")
+            self.frames = self.frames[self.frames.turn_signal == 1]
 
     def __getitem__(self, idx):
         frame = self.frames.iloc[idx]
@@ -128,6 +132,9 @@ class NvidiaDataset(Dataset):
         frames_df = frames_df[frames_df['vehicle_speed'].notna()]
         frames_df = frames_df[frames_df[f'{camera}_filename'].notna()]
 
+        frames_df["turn_signal"].fillna(1, inplace=True)
+        frames_df["turn_signal"] = frames_df["turn_signal"].astype(int)
+
         # vehicle_x = frames_df["position_x"]
         # vehicle_y = frames_df["position_y"]
         # for i in np.arange(1, self.N_WAYPOINTS + 1):
@@ -150,8 +157,12 @@ class NvidiaDataset(Dataset):
         print(f"{dataset_path}: {len(frames_df)}")
         return frames_df
 
+    def steering_angles_degrees(self):
+        return self.frames.steering_angle.to_numpy() / np.pi * 180
+
+
 class NvidiaTrainDataset(NvidiaDataset):
-    def __init__(self, root_path):
+    def __init__(self, root_path, filter_turns=False):
         train_paths = [
             root_path / "2021-05-20-12-36-10_e2e_sulaoja_20_30",
             root_path / "2021-05-20-12-43-17_e2e_sulaoja_20_30",
@@ -161,9 +172,6 @@ class NvidiaTrainDataset(NvidiaDataset):
             root_path / "2021-05-20-13-59-00_e2e_sulaoja_10_10",
             root_path / "2021-05-28-15-07-56_e2e_sulaoja_20_30",
             root_path / "2021-05-28-15-17-19_e2e_sulaoja_20_30",
-            root_path / "2021-06-07-14-06-31_e2e_rec_ss6",
-            root_path / "2021-06-07-14-09-18_e2e_rec_ss6",
-            root_path / "2021-06-07-14-36-16_e2e_rec_ss6",
             root_path / "2021-06-09-13-14-51_e2e_rec_ss2",
             root_path / "2021-06-09-13-55-03_e2e_rec_ss2_backwards",
             root_path / "2021-06-09-14-58-11_e2e_rec_ss3",
@@ -178,6 +186,7 @@ class NvidiaTrainDataset(NvidiaDataset):
             root_path / "2021-06-10-15-03-16_e2e_ss3_backwards",
             root_path / "2021-06-14-11-08-19_e2e_rec_ss14",
             root_path / "2021-06-14-11-22-05_e2e_rec_ss14",
+            root_path / "2021-06-14-11-22-05_e2e_rec_ss14",
             root_path / "2021-06-14-11-43-48_e2e_rec_ss14_backwards",
             root_path / "2021-09-24-11-19-25_e2e_rec_ss10",
             root_path / "2021-09-24-11-40-24_e2e_rec_ss10_2",
@@ -187,33 +196,48 @@ class NvidiaTrainDataset(NvidiaDataset):
             root_path / "2021-09-30-13-57-00_e2e_rec_ss14",
             root_path / "2021-09-30-15-03-37_e2e_ss14_from_half_way",
             root_path / "2021-09-30-15-20-14_e2e_ss14_backwards",
+            root_path / "2021-09-30-15-56-59_e2e_ss14_attempt_2",
+            root_path / "2021-10-07-11-05-13_e2e_rec_ss3",
             root_path / "2021-10-07-11-44-52_e2e_rec_ss3_backwards",
+            root_path / "2021-10-07-12-54-17_e2e_rec_ss4",
             root_path / "2021-10-07-13-22-35_e2e_rec_ss4_backwards",
+            root_path / "2021-10-11-16-06-44_e2e_rec_ss2",
             root_path / "2021-10-11-17-10-23_e2e_rec_last_part",
             root_path / "2021-10-11-17-14-40_e2e_rec_backwards",
-            root_path / "2021-10-11-17-20-12_e2e_rec_backwards"
-
+            root_path / "2021-10-11-17-20-12_e2e_rec_backwards",
+            root_path / "2021-10-20-14-55-47_e2e_rec_vastse_ss13_17",
+            root_path / "2021-10-20-13-57-51_e2e_rec_neeruti_ss19_22",
+            root_path / "2021-10-20-14-15-07_e2e_rec_neeruti_ss19_22_back",
+            root_path / "2021-10-25-17-31-48_e2e_rec_ss2_arula",
+            root_path / "2021-10-25-17-06-34_e2e_rec_ss2_arula_back"
         ]
 
         tr = transforms.Compose([NvidiaCropWide(), Normalize()])
 
-        super().__init__(train_paths, tr)
+        super().__init__(train_paths, tr, filter_turns=filter_turns)
 
 class NvidiaValidationDataset(NvidiaDataset):
-    def __init__(self, root_path):
+    def __init__(self, root_path, filter_turns=False):
         valid_paths = [
             root_path / "2021-05-28-15-19-48_e2e_sulaoja_20_30",
             root_path / "2021-06-07-14-20-07_e2e_rec_ss6",
-            root_path / "2021-06-14-11-22-05_e2e_rec_ss14",
-            root_path / "2021-10-07-11-05-13_e2e_rec_ss3",
-            root_path / "2021-09-24-14-03-45_e2e_rec_ss11_backwards"
+            root_path / "2021-06-07-14-06-31_e2e_rec_ss6",
+            root_path / "2021-06-07-14-09-18_e2e_rec_ss6",
+            root_path / "2021-06-07-14-36-16_e2e_rec_ss6",
+            root_path / "2021-09-24-14-03-45_e2e_rec_ss11_backwards",
+            root_path / "2021-10-26-10-49-06_e2e_rec_ss20_elva",
+            root_path / "2021-10-26-11-08-59_e2e_rec_ss20_elva_back",
+            root_path / "2021-10-20-15-11-29_e2e_rec_vastse_ss13_17_back",
+            root_path / "2021-10-11-14-50-59_e2e_rec_vahi",
+            root_path / "2021-10-14-13-08-51_e2e_rec_vahi_backwards"
+
         ]
 
         tr = transforms.Compose([NvidiaCropWide(), Normalize()])
-        super().__init__(valid_paths, tr)
+        super().__init__(valid_paths, tr, filter_turns=filter_turns)
 
 class NvidiaSpringTrainDataset(NvidiaDataset):
-    def __init__(self, root_path):
+    def __init__(self, root_path, filter_turns=False):
         train_paths = [
             root_path / "2021-05-20-12-36-10_e2e_sulaoja_20_30",
             root_path / "2021-05-20-12-43-17_e2e_sulaoja_20_30",
@@ -244,6 +268,6 @@ class NvidiaSpringTrainDataset(NvidiaDataset):
 
         tr = transforms.Compose([NvidiaCropWide(), Normalize()])
 
-        super().__init__(train_paths, tr)
+        super().__init__(train_paths, tr, filter_turns=filter_turns)
 
 
--- dataloading/ouster.py
+++ dataloading/ouster.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 
 import torchvision
+from torchvision import transforms
+
 from torch.utils.data import Dataset
 
 
@@ -27,16 +29,17 @@ class OusterNormalize(object):
 
 
 class OusterDataset(Dataset):
-    N_WAYPOINTS = 5
-    CAP_WAYPOINTS = 10
-
-    def __init__(self, dataset_paths, transform=None):
+    def __init__(self, dataset_paths, transform=None, filter_turns=False):
 
         self.dataset_paths = dataset_paths
         self.transform = transform
 
         datasets = [self.read_dataset(dataset_path) for dataset_path in dataset_paths]
         self.frames = pd.concat(datasets)
+
+        if filter_turns:
+            print("Filtering turns with blinker signal")
+            self.frames = self.frames[self.frames.turn_signal == 1]
 
     def __getitem__(self, idx):
         frame = self.frames.iloc[idx]
@@ -45,12 +48,7 @@ class OusterDataset(Dataset):
         data = {
             'image': image,
             'steering_angle': np.array(frame["steering_angle"]),
-            'vehicle_speed': np.array(frame["vehicle_speed"]),
-            'waypoints': np.array([frame["x_1_offset"], frame["y_1_offset"],
-                                   frame["x_2_offset"], frame["y_2_offset"],
-                                   frame["x_3_offset"], frame["y_3_offset"],
-                                   frame["x_4_offset"], frame["y_4_offset"],
-                                   frame["x_5_offset"], frame["y_5_offset"]])
+            'vehicle_speed': np.array(frame["vehicle_speed"])
         }
 
         if self.transform:
@@ -73,22 +71,87 @@ class OusterDataset(Dataset):
         frames_df = frames_df[frames_df['vehicle_speed'].notna()]
         frames_df = frames_df[frames_df['lidar_filename'].notna()]
 
-        vehicle_x = frames_df["position_x"]
-        vehicle_y = frames_df["position_y"]
-        for i in np.arange(1, self.N_WAYPOINTS + 1):
-            wp_global_x = frames_df["position_x"].shift(-i * self.CAP_WAYPOINTS)
-            wp_global_y = frames_df["position_y"].shift(-i * self.CAP_WAYPOINTS)
-            yaw = frames_df["yaw"]
-
-            wp_local_x = (wp_global_x - vehicle_x) * np.cos(yaw) + (wp_global_y - vehicle_y) * np.sin(yaw)
-            wp_local_y = -(wp_global_x - vehicle_x) * np.sin(yaw) + (wp_global_y - vehicle_y) * np.cos(yaw)
-            frames_df[f"x_{i}_offset"] = wp_local_x
-            frames_df[f"y_{i}_offset"] = wp_local_y
-
-            # Remove rows without trajectory offsets, should be last N_WAYPOINTS rows
-            frames_df = frames_df[frames_df[f"x_{i}_offset"].notna()]
+        frames_df["turn_signal"].fillna(1, inplace=True)
+        frames_df["turn_signal"] = frames_df["turn_signal"].astype(int)
 
         camera_images = frames_df["lidar_filename"].to_numpy()
         frames_df["image_path"] = [str(dataset_path / image_path) for image_path in camera_images]
 
+        print(f"{dataset_path}: {len(frames_df)}")
         return frames_df
+
+
+class OusterTrainDataset(OusterDataset):
+    def __init__(self, root_path, filter_turns=False):
+        train_paths = [
+            root_path / "2021-05-20-12-36-10_e2e_sulaoja_20_30",
+            root_path / "2021-05-20-12-43-17_e2e_sulaoja_20_30",
+            root_path / "2021-05-20-12-51-29_e2e_sulaoja_20_30",
+            root_path / "2021-05-20-13-44-06_e2e_sulaoja_10_10",
+            root_path / "2021-05-20-13-51-21_e2e_sulaoja_10_10",
+            root_path / "2021-05-20-13-59-00_e2e_sulaoja_10_10",
+            root_path / "2021-05-28-15-07-56_e2e_sulaoja_20_30",
+            root_path / "2021-05-28-15-17-19_e2e_sulaoja_20_30",
+            root_path / "2021-06-09-13-14-51_e2e_rec_ss2",
+            root_path / "2021-06-09-13-55-03_e2e_rec_ss2_backwards",
+            root_path / "2021-06-09-14-58-11_e2e_rec_ss3",
+            root_path / "2021-06-09-15-42-05_e2e_rec_ss3_backwards",
+            root_path / "2021-06-09-16-24-59_e2e_rec_ss13",
+            root_path / "2021-06-09-16-50-22_e2e_rec_ss13_backwards",
+            root_path / "2021-06-10-12-59-59_e2e_ss4",
+            root_path / "2021-06-10-13-19-22_e2e_ss4_backwards",
+            root_path / "2021-06-10-13-51-34_e2e_ss12",
+            root_path / "2021-06-10-14-02-24_e2e_ss12_backwards",
+            root_path / "2021-06-10-14-44-24_e2e_ss3_backwards",
+            root_path / "2021-06-10-15-03-16_e2e_ss3_backwards",
+            root_path / "2021-06-14-11-08-19_e2e_rec_ss14",
+            root_path / "2021-06-14-11-22-05_e2e_rec_ss14",
+            root_path / "2021-06-14-11-22-05_e2e_rec_ss14",
+            root_path / "2021-06-14-11-43-48_e2e_rec_ss14_backwards",
+            root_path / "2021-09-24-11-19-25_e2e_rec_ss10",
+            root_path / "2021-09-24-11-40-24_e2e_rec_ss10_2",
+            root_path / "2021-09-24-12-02-32_e2e_rec_ss10_3",
+            root_path / "2021-09-24-12-21-20_e2e_rec_ss10_backwards",
+            root_path / "2021-09-24-13-39-38_e2e_rec_ss11",
+            root_path / "2021-09-30-13-57-00_e2e_rec_ss14",
+            root_path / "2021-09-30-15-03-37_e2e_ss14_from_half_way",
+            root_path / "2021-09-30-15-20-14_e2e_ss14_backwards",
+            root_path / "2021-09-30-15-56-59_e2e_ss14_attempt_2",
+            root_path / "2021-10-07-11-05-13_e2e_rec_ss3",
+            root_path / "2021-10-07-11-44-52_e2e_rec_ss3_backwards",
+            root_path / "2021-10-07-12-54-17_e2e_rec_ss4",
+            root_path / "2021-10-07-13-22-35_e2e_rec_ss4_backwards",
+            root_path / "2021-10-11-16-06-44_e2e_rec_ss2",
+            root_path / "2021-10-11-17-10-23_e2e_rec_last_part",
+            root_path / "2021-10-11-17-14-40_e2e_rec_backwards",
+            root_path / "2021-10-11-17-20-12_e2e_rec_backwards",
+            root_path / "2021-10-20-14-55-47_e2e_rec_vastse_ss13_17",
+            root_path / "2021-10-20-13-57-51_e2e_rec_neeruti_ss19_22",
+            root_path / "2021-10-20-14-15-07_e2e_rec_neeruti_ss19_22_back",
+            root_path / "2021-10-25-17-31-48_e2e_rec_ss2_arula",
+            root_path / "2021-10-25-17-06-34_e2e_rec_ss2_arula_back"
+        ]
+
+        tr = transforms.Compose([OusterCrop(), OusterNormalize()])
+
+        super().__init__(train_paths, tr, filter_turns=filter_turns)
+
+class OusterValidationDataset(OusterDataset):
+    def __init__(self, root_path, filter_turns=False):
+        valid_paths = [
+            root_path / "2021-05-28-15-19-48_e2e_sulaoja_20_30",
+            root_path / "2021-06-07-14-20-07_e2e_rec_ss6",
+            root_path / "2021-06-07-14-06-31_e2e_rec_ss6",
+            root_path / "2021-06-07-14-09-18_e2e_rec_ss6",
+            root_path / "2021-06-07-14-36-16_e2e_rec_ss6",
+            root_path / "2021-09-24-14-03-45_e2e_rec_ss11_backwards",
+            root_path / "2021-10-26-10-49-06_e2e_rec_ss20_elva",
+            root_path / "2021-10-26-11-08-59_e2e_rec_ss20_elva_back",
+            root_path / "2021-10-20-15-11-29_e2e_rec_vastse_ss13_17_back",
+            root_path / "2021-10-11-14-50-59_e2e_rec_vahi",
+            root_path / "2021-10-14-13-08-51_e2e_rec_vahi_backwards"
+
+        ]
+
+        tr = transforms.Compose([OusterCrop(), OusterNormalize()])
+        super().__init__(valid_paths, tr, filter_turns=filter_turns)
