e3bc58167fcf30e754dd0f02bb542dcf5d73b81c
Romet Aidla <romet.aidla@gmail.com>
2021-12-06 10:36:43 +0200

    Metrics job fixed.

--- metrics/closed_loop_metrics_elva.sh
+++ metrics/closed_loop_metrics_elva.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
 echo "Camera v1"
-python ../closed_loop_metrics.py --input-modality nvidia --drive-datasets 2021-11-25-12-09-43_e2e_rec_elva-nvidia-v1-0.8 2021-11-25-12-21-17_e2e_rec_elva-nvidia-v1-0.8-forward
+python metrics.py --input-modality nvidia --drive-datasets 2021-11-25-12-09-43_e2e_rec_elva-nvidia-v1-0.8 2021-11-25-12-21-17_e2e_rec_elva-nvidia-v1-0.8-forward
 echo "Camera v2"
-python ../closed_loop_metrics.py --input-modality nvidia --drive-datasets 2021-11-25-14-01-46_e2e_rec_elva-licamera-v2-0.8-back 2021-11-25-14-13-59_e2e_rec_elva-licamera-v2-0.8-forward
+python metrics.py --input-modality nvidia --drive-datasets 2021-11-25-14-01-46_e2e_rec_elva-licamera-v2-0.8-back 2021-11-25-14-13-59_e2e_rec_elva-licamera-v2-0.8-forward
 echo "Camera v3"
-python ../closed_loop_metrics.py --input-modality nvidia --drive-datasets 2021-11-25-14-51-46_e2e_rec_elva-l-camera-v3-0.8-back 2021-11-25-15-04-26_e2e_rec_elva-l-camera-v3-0.8-forward
+python metrics.py --input-modality nvidia --drive-datasets 2021-11-25-14-51-46_e2e_rec_elva-l-camera-v3-0.8-back 2021-11-25-15-04-26_e2e_rec_elva-l-camera-v3-0.8-forward
 echo "Camera in train"
-python ../closed_loop_metrics.py --input-modality nvidia --drive-datasets 2021-11-25-13-11-40_e2e_rec_elva-licamera-inTrain-0.8-back 2021-11-25-13-24-00_e2e_rec_elva-licamera-inTrain-0.8-forward
+python metrics.py --input-modality nvidia --drive-datasets 2021-11-25-13-11-40_e2e_rec_elva-licamera-inTrain-0.8-back 2021-11-25-13-24-00_e2e_rec_elva-licamera-inTrain-0.8-forward
 
 echo "Lidar v1"
-python ../closed_loop_metrics.py --input-modality lidar --drive-datasets 2021-11-25-12-37-01_e2e_rec_elva-lidar-v1-0.8-back 2021-11-25-12-45-35_e2e_rec_elva-lidar-v1-0.8-back 2021-11-25-12-57-24_e2e_rec_elva-lidar-v1-0.8-forward
+python metrics.py --input-modality lidar --drive-datasets 2021-11-25-12-37-01_e2e_rec_elva-lidar-v1-0.8-back 2021-11-25-12-45-35_e2e_rec_elva-lidar-v1-0.8-back 2021-11-25-12-57-24_e2e_rec_elva-lidar-v1-0.8-forward
 echo "Lidar v2"
-python ../closed_loop_metrics.py --input-modality lidar --drive-datasets 2021-11-25-14-27-56_e2e_rec_elva-lidar-v2-0.8-back 2021-11-25-14-39-43_e2e_rec_elva-lidar-v2-0.8-forward
+python metrics.py --input-modality lidar --drive-datasets 2021-11-25-14-27-56_e2e_rec_elva-lidar-v2-0.8-back 2021-11-25-14-39-43_e2e_rec_elva-lidar-v2-0.8-forward
 echo "Lidar v3"
-python ../closed_loop_metrics.py --input-modality lidar --drive-datasets 2021-11-25-15-16-31_e2e_rec_elva-l-lidar-v3-0.8-back 2021-11-25-15-27-38_e2e_rec_elva-l-lidar-v3-0.8-forward
+python metrics.py --input-modality lidar --drive-datasets 2021-11-25-15-16-31_e2e_rec_elva-l-lidar-v3-0.8-back 2021-11-25-15-27-38_e2e_rec_elva-l-lidar-v3-0.8-forward
 echo "Lidar in train"
-python ../closed_loop_metrics.py --input-modality lidar --drive-datasets 2021-11-25-13-37-42_e2e_rec_elva-lilidar-inTrain-0.8-back 2021-11-25-13-48-44_e2e_rec_elva-lilidar-inTrain-0.8-forward
+python metrics.py --input-modality lidar --drive-datasets 2021-11-25-13-37-42_e2e_rec_elva-lilidar-inTrain-0.8-back 2021-11-25-13-48-44_e2e_rec_elva-lilidar-inTrain-0.8-forward
 echo "Lidar night"
-python ../closed_loop_metrics.py --input-modality lidar --drive-datasets 2021-11-25-16-57-26_e2e_rec_elva-lidar-inTrain-0.8-forwardNight 2021-11-25-17-08-28_e2e_rec_elva-lidar-inTrain-0.8-backNight
+python metrics.py --input-modality lidar --drive-datasets 2021-11-25-16-57-26_e2e_rec_elva-lidar-inTrain-0.8-forwardNight 2021-11-25-17-08-28_e2e_rec_elva-lidar-inTrain-0.8-backNight
 echo "Lidar in train night"
-python ../closed_loop_metrics.py --input-modality lidar --drive-datasets 2021-11-25-17-20-55_e2e_rec_elva-lidar-0.8-forwardNight 2021-11-25-17-31-42_e2e_rec_elva-lidar-0.8-forwardNight 2021-11-25-17-43-47_e2e_rec_elva-lidar-0.8-backNight
+python metrics.py --input-modality lidar --drive-datasets 2021-11-25-17-20-55_e2e_rec_elva-lidar-0.8-forwardNight 2021-11-25-17-31-42_e2e_rec_elva-lidar-0.8-forwardNight 2021-11-25-17-43-47_e2e_rec_elva-lidar-0.8-backNight
 echo "Lidar in train night #2"
-python ../closed_loop_metrics.py --input-modality lidar --drive-datasets 2021-11-25-17-56-16_e2e_rec_elva-lidar-0.8-forwardNight_attempt2 2021-11-25-18-07-28_e2e_rec_elva-lidar-0.8-backNight_attempt2
+python metrics.py --input-modality lidar --drive-datasets 2021-11-25-17-56-16_e2e_rec_elva-lidar-0.8-forwardNight_attempt2 2021-11-25-18-07-28_e2e_rec_elva-lidar-0.8-backNight_attempt2
 
 echo "Lidar all channels"
-python ../closed_loop_metrics.py --input-modality lidar --drive-datasets 2021-11-26-11-19-15_e2e_rec_elva_i_allChannels_forward_0.8 2021-11-26-11-30-23_e2e_rec_elva_i_allChannels_back_0.8
+python metrics.py --input-modality lidar --drive-datasets 2021-11-26-11-19-15_e2e_rec_elva_i_allChannels_forward_0.8 2021-11-26-11-30-23_e2e_rec_elva_i_allChannels_back_0.8
 echo "Lidar intensity"
-python ../closed_loop_metrics.py --input-modality lidar --drive-datasets 2021-11-26-10-53-35_e2e_rec_elva_intensity_forward_0.8 2021-11-26-11-07-10_e2e_rec_elva_intensity_back_0.8
+python metrics.py --input-modality lidar --drive-datasets 2021-11-26-10-53-35_e2e_rec_elva_intensity_forward_0.8 2021-11-26-11-07-10_e2e_rec_elva_intensity_back_0.8
 echo "Lidar range"
-python ../closed_loop_metrics.py --input-modality lidar --drive-datasets 2021-11-26-11-42-02_e2e_rec_elva_i_range_forward_0.8
+python metrics.py --input-modality lidar --drive-datasets 2021-11-26-11-42-02_e2e_rec_elva_i_range_forward_0.8
 echo "Lidar ambience"
-python ../closed_loop_metrics.py --input-modality lidar --drive-datasets 2021-11-26-11-53-18_e2e_rec_elva_i_ambience_forward_0.8
+python metrics.py --input-modality lidar --drive-datasets 2021-11-26-11-53-18_e2e_rec_elva_i_ambience_forward_0.8
 
 
---
+++ metrics/metrics.py
@@ -0,0 +1,132 @@
+import argparse
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import BallTree
+
+
+def calculate_closed_loop_metrics(model_frames, expert_frames, fps=30, failure_rate_threshold=1.0,
+                                  only_autonomous=True):
+    model_steering = model_frames.steering_angle.to_numpy() / np.pi * 180
+    true_steering = expert_frames.steering_angle.to_numpy() / np.pi * 180
+
+    lat_errors = calculate_lateral_errors(model_frames, expert_frames, only_autonomous)
+    whiteness = calculate_whiteness(model_steering, fps)
+    expert_whiteness = calculate_whiteness(true_steering, fps)
+
+    max = lat_errors.max()
+    mae = lat_errors.mean()
+    rmse = np.sqrt((lat_errors ** 2).mean())
+    failure_rate = len(lat_errors[lat_errors > failure_rate_threshold]) / float(len(lat_errors)) * 100
+    interventions = calculate_interventions(model_frames)
+
+    return {
+        'mae': mae,
+        'rmse': rmse,
+        'max': max,
+        'failure_rate': failure_rate,
+        'interventions': interventions,
+        'whiteness': whiteness,
+        'expert_whiteness': expert_whiteness,
+    }
+
+def calculate_open_loop_metrics(predicted_steering, true_steering, fps):
+    predicted_degrees = np.array(predicted_steering) / np.pi * 180
+    true_degrees = true_steering / np.pi * 180
+    errors = np.abs(true_degrees - predicted_degrees)
+    mae = errors.mean()
+    rmse = np.sqrt((errors ** 2).mean())
+    max = errors.max()
+
+    whiteness = calculate_whiteness(predicted_degrees, fps)
+    expert_whiteness = calculate_whiteness(true_degrees, fps)
+
+    return {
+        'mae': mae,
+        'rmse': rmse,
+        'max': max,
+        'whiteness': whiteness,
+        'expert_whiteness': expert_whiteness
+    }
+
+
+def calculate_whiteness(steering_angles, fps=30):
+    current_angles = steering_angles[:-1]
+    next_angles = steering_angles[1:]
+    delta_angles = next_angles - current_angles
+    whiteness = np.sqrt(((delta_angles * fps) ** 2).mean())
+    return whiteness
+
+
+def calculate_lateral_errors(model_frames, expert_frames, only_autonomous=True):
+    model_trajectory_df = model_frames[["position_x", "position_y", "autonomous"]].rename(
+        columns={"position_x": "X", "position_y": "Y"})
+    expert_trajectory_df = expert_frames[["position_x", "position_y", "autonomous"]].rename(
+        columns={"position_x": "X", "position_y": "Y"})
+
+    if only_autonomous:
+        model_trajectory_df = model_trajectory_df[model_trajectory_df.autonomous].reset_index(drop=True)
+
+    tree = BallTree(expert_trajectory_df.values)
+    inds, dists = tree.query_radius(model_trajectory_df.values, r=2, sort_results=True, return_distance=True)
+    closest_l = []
+    for i, ind in enumerate(inds):
+        if len(ind) >= 2:
+            closest = pd.DataFrame({
+                'X1': [expert_trajectory_df.iloc[ind[0]].X],
+                'Y1': [expert_trajectory_df.iloc[ind[0]].Y],
+                'X2': [expert_trajectory_df.iloc[ind[1]].X],
+                'Y2': [expert_trajectory_df.iloc[ind[1]].Y]},
+                index=[i])
+            closest_l.append(closest)
+    closest_df = pd.concat(closest_l)
+    f = model_trajectory_df.join(closest_df)
+    lat_errors = abs((f.X2 - f.X1) * (f.Y1 - f.Y) - (f.X1 - f.X) * (f.Y2 - f.Y1)) / np.sqrt(
+        (f.X2 - f.X1) ** 2 + (f.Y2 - f.Y1) ** 2)
+    # lat_errors.dropna(inplace=True)  # Why na-s?
+
+    return lat_errors
+
+
+def calculate_interventions(frames):
+    frames['autonomous_next'] = frames.shift(-1)['autonomous']
+    return len(frames[frames.autonomous & (frames.autonomous_next == False)])
+
+
+def read_frames(dataset_paths, modality="nvidia"):
+    datasets = [pd.read_csv(dataset_path / f"{modality}_frames.csv") for dataset_path in dataset_paths]
+    return pd.concat(datasets)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--root-path",
+                        default="/gpfs/space/projects/Bolt/dataset",
+                        help='Path to extracted datasets')
+    parser.add_argument('--expert-datasets',
+                        nargs='+',
+                        default=['2021-10-26-10-49-06_e2e_rec_ss20_elva',
+                                 '2021-10-26-11-08-59_e2e_rec_ss20_elva_back'],
+                        help='Datasets used for ground truth tracjectories.')
+    parser.add_argument('--drive-datasets',
+                        nargs='+',
+                        required=True,
+                        default=[],
+                        help='Datasets used to calculate metrics for.')
+    parser.add_argument('--input-modality',
+                        default='nvidia',
+                        help='Input modality used for driving')
+
+    args = parser.parse_args()
+
+    root_path = Path(args.root_path)
+
+    expert_ds = [root_path / dataset_path for dataset_path in args.expert_datasets]
+    expert_frames = read_frames(expert_ds, args.input_modality)
+
+    drive_ds = [root_path / dataset_path for dataset_path in args.drive_datasets]
+    model_frames = read_frames(drive_ds, args.input_modality)
+
+    print(calculate_closed_loop_metrics(model_frames, expert_frames))
