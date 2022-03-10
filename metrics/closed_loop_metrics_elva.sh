36ddbe3c105beb8d49b330ef38722946bbe53738
Romet Aidla <romet.aidla@ut.ee>
2022-03-10 16:21:18 +0200

    Fixed wrong bags used for winter experiments.

--- metrics/closed_loop_metrics_elva.sh
+++ metrics/closed_loop_metrics_elva.sh
@@ -34,11 +34,11 @@ echo "Lidar ambience"
 python metrics.py --root-path /gpfs/space/projects/Bolt/dataset-paper --input-modality ouster-lidar --drive-datasets 2021-11-26-11-53-18_e2e_rec_elva_i_ambience_forward_0.8
 
 echo "Lidar Winter v1"
-python metrics.py --root-path /gpfs/space/projects/Bolt/dataset-paper --input-modality ouster-lidar --drive-datasets 2022-02-02-11-32-37_e2e_rec_elva_winter_lidar-v3_forw_08 2022-02-02-11-45-34_e2e_rec_elva_winter_lidar-v3_backw_08
-echo "Lidar Winter v2"
 python metrics.py --root-path /gpfs/space/projects/Bolt/dataset-paper --input-modality ouster-lidar --drive-datasets 2022-02-02-10-39-23_e2e_rec_elva_winter_lidar_forward_08 2022-02-02-10-50-07_e2e_rec_elva_winter_lidar_forward_08 2022-02-02-10-53-16_e2e_rec_elva_winter_lidar_backw_08
+echo "Lidar Winter v2"
+python metrics.py --root-path /gpfs/space/projects/Bolt/dataset-paper --input-modality ouster-lidar --drive-datasets 2022-02-02-11-05-18_e2e_rec_elva_winter_lidar-v5_forw_08 2022-02-02-11-18-14_e2e_rec_elva_winter_lidar-v5_backw_08
 echo "Lidar Winter v3"
-python metrics.py --root-path /gpfs/space/projects/Bolt/dataset-paper --input-modality ouster-lidar --drive-datasets 2022-02-02-11-05-18_e2e_rec_elva_winter_lidar-v5_forw_08 2022-02-02-10-53-16_e2e_rec_elva_winter_lidar_backw_08
+python metrics.py --root-path /gpfs/space/projects/Bolt/dataset-paper --input-modality ouster-lidar --drive-datasets 2022-02-02-11-32-37_e2e_rec_elva_winter_lidar-v3_forw_08 2022-02-02-11-45-34_e2e_rec_elva_winter_lidar-v3_backw_08
 echo "Camera Winter v3"
 python metrics.py --root-path /gpfs/space/projects/Bolt/dataset-paper --input-modality ouster-lidar --drive-datasets 2022-02-02-11-58-48_e2e_rec_elva_winter_camera-v3_forw_08
 
