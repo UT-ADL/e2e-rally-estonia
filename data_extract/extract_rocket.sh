d53765a8fd05f3bd1b41ffb88cd0a3eb30598fe7
Romet Aidla <romet.aidla@ut.ee>
2022-05-05 14:08:20 +0300

    Lidar files are extracted in rocket by default.

--- data_extract/extract_rocket.sh
+++ data_extract/extract_rocket.sh
@@ -2,4 +2,4 @@
 
 echo "Bag file: $1"
 
-python3 ./image_extractor.py --bag-file=/gpfs/space/projects/Bolt/bagfiles/$1 --extract-dir=$2
+python3 ./image_extractor.py --extract-lidar --bag-file=/gpfs/space/projects/Bolt/bagfiles/$1 --extract-dir=$2
