08492d538b53fc05e8dce5b9e1482010fe5e86ed
Romet Aidla <romet.aidla@ut.ee>
2022-05-16 17:50:56 +0300

    Script for steering angle error plot added.

--- viz/analytics.py
+++ viz/analytics.py
@@ -1,24 +1,27 @@
+import math
+
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 
 import torch
+from tqdm.auto import tqdm
 
 from dataloading.nvidia import NvidiaDataset, Normalize
 
-from trainer import Trainer
 from torchvision import transforms
+
+from trajectory import calculate_steering_angle
 
 N_WAYPOINTS = 10
 
 
-def create_waypoint_error_plot(model, dataset_name, n_branches=3, n_waypoints=6):
+def create_waypoint_error_plot(model, trainer, dataset_name, n_branches=3, n_waypoints=6):
     root_path = Path("/home/romet/data2/datasets/rally-estonia/dataset-new-small/summer2021")
     tr = transforms.Compose([Normalize()])
     dataset = NvidiaDataset([root_path / dataset_name], transform=tr, n_branches=n_branches,  n_waypoints=n_waypoints,
                             output_modality="waypoints")
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=False, num_workers=16)
-    trainer = Trainer(n_conditional_branches=n_branches)
     predictions = trainer.predict(model, dataloader)
 
     wp_x_cols = [col for col in dataset.frames.columns if col.startswith('wp_x')]
@@ -42,9 +45,20 @@ def create_waypoint_error_plot(model, dataset_name, n_branches=3, n_waypoints=6)
 
     f, (ax) = plt.subplots(3, 1, figsize=(50, 25))
     ax[0].plot(dataset.frames.steering_angle, color="green")
-    ax[0].plot(dataset.frames.turn_signal, linewidth=3, color="gold")
-    ax[0].plot(dataset.frames.vehicle_speed, color="darkorange")
-    ax[0].legend(['steering_angel', 'turn_signal', 'vehicle_speed'])
+
+    pred_steering_angles = []
+    wp_progress_bar = tqdm(total=len(predictions), smoothing=0)
+    wp_progress_bar.set_description("Calculating steering angles")
+    for wp in predictions:
+        steering_angle_wp = [0.0, 0.0]
+        steering_angle_wp.extend(wp[:10])
+        pred_steering_angles.append(calculate_steering_angle(steering_angle_wp, ref_distance=8))
+        wp_progress_bar.update(1)
+    ax[0].plot(pred_steering_angles, color="red")
+
+    #ax[0].plot(dataset.frames.turn_signal, linewidth=3, color="gold")
+    #ax[0].plot(dataset.frames.vehicle_speed, color="darkorange")
+    ax[0].legend(['true_steering_angle', 'pred_steering_angle', 'turn_signal', 'vehicle_speed'])
     ax[0].set_title(dataset_name + " | steering angle")
 
     # ax[1].plot(frechet_distances, color="darkblue")
@@ -58,4 +72,28 @@ def create_waypoint_error_plot(model, dataset_name, n_branches=3, n_waypoints=6)
     ax[2].plot(last_wp_error, color="darkgreen")
     ax[2].set_title(dataset_name + " | last waypoint error")
     ax[2].legend(['last waypoint error'])
+
+
+def create_steering_angle_error_plot(model, trainer, dataset_name, n_branches=3):
+    root_path = Path("/home/romet/data2/datasets/rally-estonia/dataset-new-small/summer2021")
+    tr = transforms.Compose([Normalize()])
+    dataset = NvidiaDataset([root_path / dataset_name], transform=tr, n_branches=n_branches, output_modality="steering_angle")
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=False, num_workers=16)
+    pred_steering_angles = trainer.predict(model, dataloader)
+
+    f, (ax) = plt.subplots(2, 1, figsize=(50, 25))
+    true_steering_angle = dataset.frames.steering_angle
+    ax[0].plot(true_steering_angle, color="green")
+    ax[0].plot(pred_steering_angles, color="red")
+
+    ax[0].plot(dataset.frames.turn_signal, linewidth=3, color="gold")
+    ax[0].plot(dataset.frames.vehicle_speed, color="darkorange")
+    ax[0].legend(['true_steering_angle', 'pred_steering_angle', 'turn_signal', 'vehicle_speed'])
+    ax[0].set_title(dataset_name + " | steering angle")
+
+    steering_error = np.abs(pred_steering_angles - true_steering_angle)
+    ax[1].plot(steering_error, color="darkred")
+    ax[1].set_title(dataset_name + " | steering error")
+    ax[1].legend(['steering error'])
+
 
