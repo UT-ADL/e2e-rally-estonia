9073a852e6e6b122cbfa83893681fd513a50c3b5
Romet Aidla <romet.aidla@ut.ee>
2022-05-25 12:46:55 +0300

    Lidar and side camera images are extracted for cropped dataset.

--- data_extract/extract_rocket_small.sh
+++ data_extract/extract_rocket_small.sh
@@ -2,4 +2,4 @@
 
 echo "Bag file: $1"
 
-python3 ./image_extractor.py --bag-file=/gpfs/space/projects/Bolt/bagfiles/$1 --extract-dir=$2 --resize-camera-images --extract-lidar
+python3 ./image_extractor.py --bag-file=/gpfs/space/projects/Bolt/bagfiles/$1 --extract-dir=$2 --crop-camera-images --resize-camera-images --extract-side-cameras --extract-lidar
