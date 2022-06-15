8c257e36dea5275a8c3c24873326758568171025
Romet Aidla <rometaidla@gmail.com>
2022-06-16 01:15:14 +0300

    Wide crop adjustment.

--- data_extract/extract_rocket_wide.sh
+++ data_extract/extract_rocket_wide.sh
@@ -2,4 +2,4 @@
 
 echo "Bag file: $1"
 
-python3 ./image_extractor.py --bag-file=/gpfs/space/projects/Bolt/bagfiles/$1 --extract-dir=$2 --crop-camera-images --resize-camera-images --extract-side-cameras --extract-lidar --camera-crop-xmin 100 --camera-crop-xmax 1720
+python3 ./image_extractor.py --bag-file=/gpfs/space/projects/Bolt/bagfiles/$1 --extract-dir=$2 --crop-camera-images --resize-camera-images --extract-side-cameras --extract-lidar --camera-crop-xmin 0 --camera-crop-xmax 1920
