03e112c67a5042c1523faf00337378da696d545e
Romet Aidla <rometaidla@gmail.com>
2022-06-19 13:34:07 +0300

    Job extracting bags.

--- viz/create_driving_video.sh
+++ viz/create_driving_video.sh
@@ -3,4 +3,4 @@
 echo "Creating video for dataset: $1"
 
 cd ..
-python3 -m viz.video_creator --dataset-folder=/gpfs/space/projects/Bolt/dataset-paper/$1 --video-type=driving
+python3 -m viz.video_creator --dataset-folder=/gpfs/space/projects/Bolt/dataset-demo/$1 --video-type=driving
