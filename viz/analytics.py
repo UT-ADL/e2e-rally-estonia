3a6ec3881051cd4cf5657cd32f408c06f1b059d0
Romet Aidla <rometaidla@gmail.com>
2022-07-27 22:33:21 +0300

    Analytics cleaned up.

--- viz/analytics.py
+++ viz/analytics.py
@@ -14,11 +14,12 @@ from torchvision import transforms
 
 from trajectory import calculate_steering_angle
 
-def create_waypoint_error_plot(model, trainer, dataset_name, n_branches=3, n_waypoints=6):
+
+def create_waypoint_error_plot(model, trainer, dataset_name, n_branches=3):
     root_path = Path("/home/romet/data2/datasets/rally-estonia/dataset-new-small/summer2021")
     tr = transforms.Compose([Normalize()])
-    dataset = NvidiaDataset([root_path / dataset_name], transform=tr, n_branches=n_branches,  n_waypoints=n_waypoints,
-                            output_modality="waypoints")
+    dataset = NvidiaDataset([root_path / dataset_name], transform=tr, n_branches=n_branches,
+                            output_modality="waypoints", metadata_file="nvidia_frames_ext.csv")
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=False, num_workers=16)
     predictions = trainer.predict(model, dataloader)
 
@@ -38,7 +39,7 @@ def create_waypoint_error_plot(model, trainer, dataset_name, n_branches=3, n_way
     # frechet_distances = np.array(
     #     [frdist(z[0].reshape(-1, 2), z[1].reshape(-1, 2)) for z in zipped_waypoints])
 
-    f, (ax) = plt.subplots(6, 1, figsize=(50, 25))
+    f, (ax) = plt.subplots(3, 1, figsize=(50, 25))
     ax[0].plot(dataset.frames.steering_angle, color="green")
 
     pred_steering_angles = []
@@ -69,17 +70,6 @@ def create_waypoint_error_plot(model, trainer, dataset_name, n_branches=3, n_way
     ax[2].plot(last_wp_error, color="darkgreen")
     ax[2].set_title(dataset_name + " | last waypoint error")
     ax[2].legend(['last waypoint error'])
-
-    first_wp_long_error = predictions[:, 0] - true_waypoints[:, 0]
-    ax[3].plot(first_wp_long_error, color="darkred")
-    ax[3].set_title(dataset_name + " | first waypoint longitudinal error")
-
-    first_wp_lat_error = predictions[:, 1] - true_waypoints[:, 1]
-    ax[4].plot(first_wp_lat_error, color="darkred")
-    ax[4].set_title(dataset_name + " | first waypoint lateral error")
-
-    ax[5].plot(true_waypoints[:, 0], color="darkred")
-    ax[5].set_title(dataset_name + " | first waypoint lateral")
 
 
 def create_steering_angle_error_plot(model, trainer, dataset_name, n_branches=3):
